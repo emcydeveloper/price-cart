b07d8584940089deb7fdc669119ab94855eaa660
emcydeveloper <emcydeveloper@gmail.com>
2022-01-25 23:38:11 +0530

    version2

--- src/DisplayItems.js
+++ src/DisplayItems.js
@@ -1,5 +1,5 @@
 import "./Style.css";
-import Card from "@mui/material/Card";
+// import Card from "@mui/material/Card";
 import Button from "@mui/material/Button";
 
 export function DisplayItems({
@@ -12,22 +12,19 @@ export function DisplayItems({
 }) {
   return (
     <div className="display-items-flex" key={key}>
-      <Card className="display-items-card">
-        <img class="item-image" alt={proName} src={proImage}></img>
+      {/* <Card className="display-items-card"> */}
 
-        <h1>{proName}</h1>
-
-        <div>{proSummary}</div>
-
-        <p className="btn-price">
-          <span className="a-price-symbol">₹</span>
-          {proPrice}
-        </p>
-
-        <Button className="btn-cart" size="small">
-          Add to cart
-        </Button>
-      </Card>
+      <img class="item-image" alt={proName} src={proImage}></img>
+      <Button className="btn-cart">Add to cart</Button>
+      <Button className="item-view-info">View Product</Button>
+      <h1>{proName}</h1>
+      <p className="btn-price">
+        <span className="a-price-symbol">₹</span>
+        {proPrice}
+      </p>
+      
+      <div>{proSummary}</div>
+      {/* </Card> */}
     </div>
   );
 }
